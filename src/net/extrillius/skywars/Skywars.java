c8ff65f1cdd6b14afe0d37b45cda524b3f4966e1
TechBug2012 <mattbstanciu@gmail.com>
2016-04-11 10:23:30 -0400

    Saving Island Data

--- src/net/extrillius/skywars/Skywars.java
+++ src/net/extrillius/skywars/Skywars.java
@@ -1,15 +1,12 @@
 package net.extrillius.skywars;
 
-import com.sk89q.worldedit.BlockVector;
-import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
 import com.sk89q.worldedit.bukkit.selections.Selection;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.flags.DefaultFlag;
-import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -19,6 +16,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mattbstanciu on 3/28/16.
@@ -28,13 +26,16 @@ public class Skywars extends JavaPlugin implements Listener {
     WorldGuardPlugin worldGuardPlugin = new WorldGuardPlugin();
     WorldEditPlugin worldEditPlugin = new WorldEditPlugin();
     private ConfigAccessor mapsAccessor;
+    private ConfigAccessor valueAccessor; // I don't even know if this file is necessary or not.
+    private List<String> valueList;
 
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this, this);
 
         this.mapsAccessor = new ConfigAccessor(this, "maps.yml");
-        this.mapsAccessor.getConfig().addDefault("maps", new ArrayList<String>()); // This is wrong!
+
+        this.valueAccessor = new ConfigAccessor(this, "values.yml"); // I don't think addDefault is necessary (maybe)
     }
 
     public WorldEditPlugin getWorldEdit() {
@@ -54,6 +55,9 @@ public class Skywars extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
         Selection s = getWorldEdit().getSelection(p);
+        if (!(sender instanceof Player)) { //There's a warning here, but it shouldn't be here.
+            sender.sendMessage("Only players can use setup commands!");
+        }
 
         if (cmd.getName().equalsIgnoreCase("create")) {
             if (args.length == 1) {
@@ -65,23 +69,31 @@ public class Skywars extends JavaPlugin implements Listener {
                         ChatColor.GREEN + " has been added.");
                 sender.sendMessage(ChatColor.GREEN + "Now define a selection and type " +
                         ChatColor.AQUA + "/island <map> <value>");
+                sender.sendMessage(ChatColor.DARK_RED + "Remember to select the middle island first!");
             }
-            if (args.length > 1) {
-                sender.sendMessage(ChatColor.RED + "Too many arguments!");
+            if (args.length > 1 || args.length < 1) {
                 sender.sendMessage(ChatColor.GRAY + "Usage: " + ChatColor.AQUA + "/create <map>");
-            }
-            if (args.length == 0) {
-                sender.sendMessage(ChatColor.GRAY + "Usage:" + ChatColor.GREEN + "/create <map>");
             }
         }
 
         if (cmd.getName().equalsIgnoreCase("island")) {
+            if (args.length > 2 || args.length < 2) {
+                sender.sendMessage(ChatColor.RED + "Usage: " + ChatColor.GRAY + "/island <map> <value>");
+            }
             s = getWorldEdit().getSelection(p);
             if (s == null) {
                 sender.sendMessage(ChatColor.RED + "Define a WorldEdit selection before running the /island command.");
             }
-            // save the blocks to file
-
+            World world = s.getWorld(); // There's a "null" warning here, but that is solved in the IF above this.
+            Location minPoint = s.getMinimumPoint();
+            Location maxPoint = s.getMaximumPoint();
+            if (!(s instanceof CuboidSelection)) {
+                sender.sendMessage(ChatColor.RED + "All you have to do is select an island as a cuboid ;)");
+            }
+            this.mapsAccessor.getConfig().set("maps." + args[0] + ".loc1", minPoint);
+            this.mapsAccessor.getConfig().set("maps." + args[0] + ".loc2", maxPoint);
+            valueList.add(args[1]);
+            this.valueAccessor.getConfig().set("values." + args[0], valueList); // iffy
         }
 
         if (cmd.getName().equalsIgnoreCase("delete")) {
